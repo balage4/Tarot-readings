1a2d070cf73a42bd3640ea2633b16b6d861a78ea
balage4 <balage4@hotmail.com>
2022-10-06 22:59:58 +0200

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import './App.scss';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Readings from './pages/Readings';
 import Navigation from './components/Navigation';
 import RoutesMapping from './common/routes/routesMapping';
 
