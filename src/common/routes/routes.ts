852554ef092c2360a8eb12c1172731ee1c43e0fd
balage4 <balage4@hotmail.com>
2022-10-06 22:59:01 +0200

    add routes

---
+++ src/common/routes/routes.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from '../../pages/Home';
+import Login from '../../pages/Login';
+import Readings from '../../pages/Readings';
+
+type singleRoute = {
+  path: string;
+  element: React.FC;
+  protected: boolean;
+};
+
+export const domRoutes: Array<singleRoute> = [
+  {
+    path: '/',
+    element: Home,
+    protected: false,
+  },
+  {
+    path: '/login',
+    element: Login,
+    protected: true,
+  },
+  {
+    path: 'readings',
+    element: Readings,
+    protected: true,
+  },
+];
